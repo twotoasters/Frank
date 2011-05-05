8f46b1d8b29b9e1e0459a4889da7f3fde8660f82
Blake Watters <blakewatters@gmail.com>
2011-05-05 17:06:26 -0400

    Kicked bundled UISpec out of the codebase. Will leverage the TT UISpec port instead (has UISpecRunner merged). Updated steps to launch via UISpecRunner (WaxSim) instead of sim_launcher.

--- gem/features-skeleton/step_definitions/launch_steps.rb
+++ gem/features-skeleton/step_definitions/launch_steps.rb
@@ -8,21 +8,20 @@ Given /^I launch the app$/ do
   rescue Timeout::Error 
   end
 
-  require 'sim_launcher'
-
-  app_path = ENV['APP_BUNDLE_PATH'] || APP_BUNDLE_PATH
+  app_path = (ENV['APP_BUNDLE_PATH'] || APP_BUNDLE_PATH).chomp
   raise "APP_BUNDLE_PATH was not set. \nPlease set a APP_BUNDLE_PATH ruby constant or environment variable to the path of your compiled Frankified iOS app bundle" if app_path.nil?
-
-  if( ENV['USE_SIM_LAUNCHER_SERVER'] )
-    simulator = SimLauncher::Client.for_iphone_app( app_path, "4.2" )
-  else
-    simulator = SimLauncher::DirectClient.for_iphone_app( app_path, "4.2" )
-  end
+  
+  require 'uispecrunner'
   
   num_timeouts = 0
   loop do
     begin
-      simulator.relaunch
+      fork do
+        config = UISpecRunner.new(:app_path => app_path, :driver => :waxsim, :family => :iphone, :sdk_version => '4.3', :verbose => true)
+        waxsim_runner = UISpecRunner::Drivers::WaxSim.new(config)
+        waxsim_runner.run_specs({})
+      end
+      
       wait_for_frank_to_come_up
       break # if we make it this far without an exception then we're good to move on
 
